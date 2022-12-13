de98712e29ddf68e286cc9aaf33bc1e500ffc612
claudiocassimiro <days_surfing@live.com>
2022-12-13 18:48:53 -0300

    feat: implements CrudService interface

---
+++ src/main/java/com/todo/todolistbackend/service/CrudService.java
@@ -0,0 +1,15 @@
+package com.todo.todolistbackend.service;
+
+import java.util.List;
+
+public interface CrudService<E> {
+    List<E> findAll();
+
+    E findById(Long id);
+
+    void insert(E entity);
+
+    void update(Long id, E entity);
+
+    void delete(Long id);
+}
